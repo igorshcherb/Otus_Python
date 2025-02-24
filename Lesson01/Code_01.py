30c39a0052284e8dbc92572aedbf7a077b46f3cd
igorshcherb <ibshch@yandex.ru>
2025-02-24 18:38:39 +0300

    Commit Lesson 01 code.

---
+++ Lesson01/Code_01.py
@@ -0,0 +1,210 @@
+print("hello world")
+hello world
+1 + 2
+3
+print()
+print('hello world!')
+hello world!
+print('The code word is "Python". Remeber it')
+The code word is "Python". Remeber it
+print("Hello, it's me!")
+Hello, it's me!
+print('"Привет, это я" по-английски будет "Hello, it\'s me"')
+"Привет, это я" по-английски будет "Hello, it's me"
+print("\"Привет, это я\" по-английски будет \"Hello, it's me\"")
+"Привет, это я" по-английски будет "Hello, it's me"
+print('''"Привет, это я" по-английски будет "Hello, it's me"''')
+"Привет, это я" по-английски будет "Hello, it's me"
+print("""кавычки одинарные: ''
+кавычки двойные: ""
+""")
+кавычки одинарные: ''
+кавычки двойные: ""
+
+print('''кавычки одинарные: ''
+кавычки двойные: ""''')
+кавычки одинарные: ''
+кавычки двойные: ""
+print('''
+кавычки одинарные: ''
+кавычки двойные: ""''')
+кавычки одинарные: ''
+кавычки двойные: ""
+print('''
+кавычки одинарные: ''
+кавычки двойные: ""
+''')
+кавычки одинарные: ''
+кавычки двойные: ""
+
+print('''\
+кавычки одинарные: ''
+кавычки двойные: ""\
+''')
+кавычки одинарные: ''
+кавычки двойные: ""
+'foo' + "bar"
+'foobar'
+
+# Печатает "hello world" (без кавычек!!)
+print("hello world")
+hello world
+"hello"
+'hello'
+123
+123
+"John"
+'John'
+1 + 2
+3
+'1' + '2'
+'12'
+"foo" + "bar"
+'foobar'
+123465168724123456167582 + 12347617823
+123465168724135803785405
+"jhkasdfjhkasfjkhasfjasdhksdjfhkasdfhj"
+'jhkasdfjhkasfjkhasfjasdhksdjfhkasdfhj'
+len("abc")
+3
+len('it\'s')
+4
+"abc" * 3
+'abcabcabc'
+len("dsfasdf" * 4 * 3)
+84
+name = "Suren"
+print("name")
+name
+print(name)
+Suren
+name = "John"
+name
+'John'
+print(name)
+John
+age = 42
+print(age)
+42
+print(42)
+print('42')
+42
+42
+type(age)
+int
+print(type(age))
+<class 'int'>
+'123' == 123
+False
+age = "fourty two"
+print(age)
+fourty two
+print(type(age))
+<class 'str'>
+2 + 2.5
+4.5
+print(type(1.5))
+<class 'float'>
+print(type(1))
+<class 'int'>
+print(type(1.0))
+<class 'float'>
+1.0 + 3.0
+4.0
+1.0 + 2.0
+3.0
+0.2 + 0.4
+0.6000000000000001
+0.1 + 0.2
+0.30000000000000004
+0.1 + 0.2 == 0.3
+False
+5 / 2
+2.5
+10 / 2
+5.0
+10 // 2
+5
+5 / 3
+1.6666666666666667
+5 // 3
+1
+19 / 20
+0.95
+19 // 20
+0
+19 % 20
+19
+21 % 20
+1
+21 / 20
+1.05
+21 // 20
+1
+41 / 20
+2.05
+41 // 20
+2
+41 % 20
+1
+2 * 3
+6
+2**10
+1024
+10 - 2
+8
+10 - 2.5
+7.5
+True
+True
+False
+False
+1 > 0
+True
+1 > 1
+False
+1 >= 1
+True
+print(type(True))
+<class 'bool'>
+isinstance(age, (str, int))
+True
+age
+'fourty two'
+isinstance(age, str)
+True
+isinstance(age, int)
+False
+isinstance(age, str | int)
+True
+isinstance(True, str)
+False
+isinstance(True, str | float)
+False
+isinstance(True, str | float | bool)
+True
+isinstance(True, str | float | bool | int)
+True
+isinstance(False, str | float | bool | int)
+True
+isinstance(False, bool | int)
+True
+isinstance(False, int)
+True
+False == 0
+True
+True == 1
+True
+True + False
+1
+True + True
+2
+False + False
+0
+print(type(bool))
+<class 'type'>
+print(type(False))
+<class 'bool'>
+False == True
+False
+
