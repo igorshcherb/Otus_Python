591fc71004ebfe14c84e9375f3bc48bcf87c72db
igorshcherb <ibshch@yandex.ru>
2025-07-23 17:48:15 +0300

    HomeWork13

---
+++ HomeWork13/store_app/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from ..models import Category, Product
+
+
+@pytest.fixture
+def category():
+    return Category.objects.create(
+        name="Тестовая категория",
+        description="Тестовая категория",
+    )
+
+
+@pytest.fixture
+def product(category):
+    return Product.objects.create(
+        name="Тестовый продукт",
+        description="Тестовый продукт",
+        price=10,
+        category=category,
+    )
