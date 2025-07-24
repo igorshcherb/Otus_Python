445fed241783db3668bd0fa76845b0f0da6fd3b9
ibshch <ibshch@yandex.ru>
2025-07-24 17:25:52 +0300

    Project

--- Project/SQL/insert/insert_connections.sql
+++ Project/SQL/insert/insert_connections.sql
@@ -4,3 +4,7 @@ insert into connections(code, connection_type_id, description, host, port, datab
   values ('pg1', 
           (SELECT ct.id FROM connection_types ct WHERE ct.code = 'postgresql'), 
           'PostgreSQL 16', '192.168.2.171', '5432', 'demo', 'postgres', 'postgres');
+insert into connections(code, connection_type_id, description, host, port, database, username, password) 
+  values ('adb', 
+          (SELECT ct.id FROM connection_types ct WHERE ct.code = 'postgresql'), 
+          'Arenadata DB', '192.168.2.151', '5432', 'adb', 'gpadmin', 'gpadmin');
